c1a76456e0bd9117e71267fe1fe22ea0bb4170eb
Harry G. Dulaney  IV <harrydulaney@gmail.com>
2020-12-17 11:52:00 -0500

    Starter complete

---
+++ src/main/java/com/spreadsheetbuddy/repository/ProjectRepository.java
@@ -0,0 +1,18 @@
+package com.spreadsheetbuddy.repository;
+
+//import com.spreadsheetbuddy.model.Project;
+//import org.springframework.data.repository.CrudRepository;
+//import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+//@Repository
+//public interface ProjectRepository extends CrudRepository<Project, String> {
+//    /**
+//     * @return a list of absolute path's to
+//     * recently opened workbook files.
+//     */
+//    List<Project> findAllByOwnerId(String ownerId);
+//
+//}
+
